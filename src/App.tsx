aa249194fb649a17977380d0100856ece88fc4fe
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-11 11:47:48 -0300

    Conclusão da Aula 04: Sorteando amigos secretos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import Configuracao from './pages/Configuracao';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Configuracao from "./pages/Configuracao";
+import Sorteio from "./pages/Sorteio";
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<Configuracao />}/>
+          <Route path="/" element={<Configuracao />} />
+          <Route path="/sorteio" element={<Sorteio />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
--- src/components/Rodape.test.tsx
+++ src/components/Rodape.test.tsx
@@ -11,10 +11,17 @@ jest.mock("../state/hooks/useListaDeParticipantes", () => {
 });
 
 const mockNavegacao = jest.fn();
+const mockSorteio = jest.fn();
 
 jest.mock("react-router-dom", () => {
   return {
     useNavigate: () => mockNavegacao,
+  };
+});
+
+jest.mock("../state/hooks/useSorteador", () => {
+  return {
+    useSorteador: () => mockSorteio,
   };
 });
 
@@ -63,5 +70,6 @@ describe("quando existem participantes sucifientes", () => {
     fireEvent.click(botao);
     expect(mockNavegacao).toHaveBeenCalledTimes(1);
     expect(mockNavegacao).toHaveBeenCalledWith("/sorteio");
+    expect(mockSorteio).toHaveBeenCalledTimes(1);
   });
 });
